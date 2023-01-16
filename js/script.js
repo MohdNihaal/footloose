07b29c98d9e84f19c8f7951d0711f9d2a54baf2e
MohdNihaal <108685239+MohdNihaal@users.noreply.github.com>
2023-01-16 19:59:37 +0530

    arrows removed in js

--- js/script.js
+++ js/script.js
@@ -8,9 +8,7 @@ $(document).ready(function(){
         speed: 500,
         fade: true,
         cssEase: 'linear',
-        arrows:true,
-        prevArrow:'<button type="button" class="slick-prev"><img style="width:60px;cursor:pointer;postion:absolute;" src="../images/left.png" /></button>',
-        nextArrow:'<button type="button" class="slick-next"><img style="width:60px;cursor:pointer;" src="../images/right.png" /></button>',
+        arrows:false,
         centerMode :true,
         autoplaySpeed: 1000,
     });
