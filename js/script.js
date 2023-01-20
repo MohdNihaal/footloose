d52b034f05c4a3676300588bcf4c58dd604e7726
MohdNihaal <abdulshihar2@gmail.com>
2023-01-20 18:04:56 +0530

    show:

--- js/script.js
+++ js/script.js
@@ -13,3 +13,18 @@ $(document).ready(function(){
         autoplaySpeed: 1000,
     });
 })
+$(document).ready(function(){
+    $("#show section.wrapper").slick({
+        autoplay:true,
+        dots:false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        arrows:false,
+        centerMode :true,
+        autoplaySpeed: 800,
+    });
+})
