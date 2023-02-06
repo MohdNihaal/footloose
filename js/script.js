c99ff0d48a47adb1ac43cb2f144a6016cc9dc550
MohdNihaal <abdulshihar2@gmail.com>
2023-02-06 19:49:59 +0530

    preloader addded

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader")
 
+    loader.classList.add("loader-hidden");
+    loader.addEventListener("transitionend", () => {
+        document.body.removeChild("loader");
+    })
+})
 
 
 $(document).ready(function(){
